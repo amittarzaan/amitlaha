c3b4ab45bee0a6d5ac664cc6dd40a7bdc2cc6f6f
Amit Laha <amitlaha@in.ibm.com>
2020-05-15 16:55:44 +0530

    commit

--- src/main/java/com/ibm/patient/PatientApplication.java
+++ src/main/java/com/ibm/patient/PatientApplication.java
@@ -14,7 +14,7 @@ public class PatientApplication {
 		SpringApplication.run(PatientApplication.class, args);
 	}
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "WorldAmitlaha15:57") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "WorldAmitlaha16:55") String name) {
         return String.format("Hello %s!", name);
 	}
 }
